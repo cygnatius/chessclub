93ae0e886f9e2f6b1dd48a2501c01e1f4d7b068a
Cygnatius <cygnatius@hotmail.com>
2017-05-10 11:22:27 +1000

    Added devise authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,6 @@
 class PagesController < ApplicationController
+	skip_before_action :authenticate_user!, only: [:index]
+	def home
+
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users do
+  end
   	resources :clubs do
   		resources :players
   		resources :matches do
   			resources :players
   		end
+    end
 		resources :tournaments do
-			resources :players
+			resources :players do
 	  		resources :matches do
 	  			resources :players
 	  		end
+      end
 		end
-  	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  	root 'pages#home'
+  root 'pages#home'
 
 end
